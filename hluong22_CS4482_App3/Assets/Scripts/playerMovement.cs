6e2cb8cd2aac0f69dcd52c1f0309401370b374ae
Michael Luong <michaeltriluong@gmail.com>
2022-12-02 00:36:40 -0500

    Added player movement script w/ min and max boundaries

--- hluong22_CS4482_App3/Assets/Scripts/playerMovement.cs
+++ hluong22_CS4482_App3/Assets/Scripts/playerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    public float speed = 5f;
+    public float minY, maxY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,28 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movement();
+    }
+
+    void movement()
+    {
+        if (Input.GetAxisRaw("Vertical") > 0f)
+        {
+            Vector3 tempPos = transform.position;
+            tempPos.y += speed * Time.deltaTime;
+
+            if (tempPos.y > maxY)
+                tempPos.y = maxY;
+            transform.position = tempPos;
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0f)
+        {
+            Vector3 tempPos = transform.position;
+            tempPos.y -= speed * Time.deltaTime;
+
+            if (tempPos.y < minY)
+                tempPos.y = minY;
+            transform.position = tempPos;
+        }
     }
 }
